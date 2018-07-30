c8ec722f530fb751dae3a79f2141bd91d2f0bf4b
Greg Trevellick <gtrevellick@googlemail.com>
2018-07-30 19:11:27 +0100

    .

--- Src/ReadMeMagic/Gulpfile.js
+++ Src/ReadMeMagic/Gulpfile.js
@@ -1,22 +1,48 @@
 ﻿/// <binding AfterBuild='default' ProjectOpened='default' />
+
 var gulp = require('gulp');
 var bower = require('gulp-bower');//not needed ?
+var replace = require('gulp-string-replace');
+
+var lineBreakRegex = "[^\n]+";
 var htmlCommentStart = "<!--";
 var htmlCommentEnd = "-->";
+
 var prefix = "Badges";
+//var start = htmlCommentStart + prefix + "START" + htmlCommentEnd + lineBreakRegex;
+//var end = lineBreakRegex + htmlCommentStart + prefix + "END" + htmlCommentEnd;
 var start = htmlCommentStart + prefix + "START" + htmlCommentEnd;
 var end = htmlCommentStart + prefix + "END" + htmlCommentEnd;
 var badgesRegex = new RegExp(start + '(.*)' + end, 'g');
-var newBadgesMarkdown = start + 'figure out line breaks' + end;
-var replace = require('gulp-string-replace');
 
-gulp.task('OpenInAbracadabra', function () { return OIAConcat('OpenInAbracadabra', 'Abracadabra') });
+var newBadgesMarkdown = start + 'figure out line breaks' + end;
+
+gulp.task('MyTaskNam', function () { return OIAConcat('MyTaskNam') });
 
 gulp.task('default',
-    ['OpenInAbracadabra']);
+    [
+        'MyTaskNam',
+    ]
+);
 
-function OIAConcat (appNam, appDesc) { 
-        gulp.src(["readme4.md"])
-            .pipe(replace(badgesRegex, newBadgesMarkdown))
-            .pipe(gulp.dest('.'));
+function OIAConcat (appNam) { 
+
+    var a = ["a", "b", "c"];
+    a.forEach(function (entry) {
+        console.log(entry);
+    });
+
+    var repos = [
+        "SolutionOpenPopUp",
+        "VsixFootie",
+        "DotNetFlags"
+    ];
+
+    repos.forEach(OIAConcat2(appNam));
+} 
+
+function OIAConcat2(appNam) {
+    gulp.src(["readme4.md"])
+        .pipe(replace(badgesRegex, newBadgesMarkdown))
+        .pipe(gulp.dest('.'));
 } 
