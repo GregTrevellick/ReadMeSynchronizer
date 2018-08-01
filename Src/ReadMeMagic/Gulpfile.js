1f0588823fc6b8ce3834758c84092170431f0193
Greg Trevellick <gtrevellick@googlemail.com>
2018-08-01 21:32:13 +0100

    working typescript

--- Src/ReadMeMagic/Gulpfile.js
+++ Src/ReadMeMagic/Gulpfile.js
@@ -4,7 +4,7 @@
 
 var gulp = require('gulp');
 ///////////////////////////var bower = require('gulp-bower');
-var replace = require('gulp-string-replace');
+//var replace = require('gulp-string-replace');
 var Server = require('karma').Server;
 var tsnode = require('ts-node');
 require('./gulpfile2.ts');
@@ -19,87 +19,87 @@ gulp.task('run_karma_tests', function (done) {
     }, done).start();
 });
 
-//Common variables
-var prefix = "Badges";
-var htmlCommentStart = "<!--" + prefix;
-var htmlCommentEnd = "-->";
-var badgeCommentStart = htmlCommentStart + "START" + htmlCommentEnd;
-var badgeCommentEnd = htmlCommentStart + "END" + htmlCommentEnd;
+////Common variables
+//var prefix = "Badges";
+//var htmlCommentStart = "<!--" + prefix;
+//var htmlCommentEnd = "-->";
+//var badgeCommentStart = htmlCommentStart + "START" + htmlCommentEnd;
+//var badgeCommentEnd = htmlCommentStart + "END" + htmlCommentEnd;
 
 //Tasks
-gulp.task('get_badge_md', function () { return ReplaceBadgeComments() });
+//gulp.task('get_badge_md', function () { return ReplaceBadgeComments() });
 
-gulp.task('default', ['get_badge_md']);
+//gulp.task('default', ['get_badge_md']);
 
-function ReplaceBadgeComments() { 
+//function ReplaceBadgeComments() { 
     
-    //tsnode(dummy());
+//    //tsnode(dummy());
 
-    var repos = [
-        "AutoFindReplace",
-        //"Badges-playground",
-        "DotNetFlags",
-        "FilesForEveryExtensionCreator",
-        "HelloWorldVstsExtension",
-        "OpenInApp.Launcher",
-        "QuickLaunchButtons",
-        "Quiz.Launcher",
-        "SolutionOpenPopUp",
-        "TrivialApisForIDE",
-        "VisualStudioMarketplaceMetrics",
-        "VsixFootie",
-        "VsixHelloWorldCommandButton",
-        "VsixHelloWorldPopUp",
-        "VsixHelloWorldToolBar",
-        "VsixRatingChaser",
-        "VsixToolWindowAsyncPackageExample",
-        "VsixTwitterWidget",
-        "VstsDashboardWidgetProjectTemplate",
-        "WpfAsyncBindingPropertyExample",
-    ];
+//    var repos = [
+//        "AutoFindReplace",
+//        //"Badges-playground",
+//        "DotNetFlags",
+//        "FilesForEveryExtensionCreator",
+//        "HelloWorldVstsExtension",
+//        "OpenInApp.Launcher",
+//        "QuickLaunchButtons",
+//        "Quiz.Launcher",
+//        "SolutionOpenPopUp",
+//        "TrivialApisForIDE",
+//        "VisualStudioMarketplaceMetrics",
+//        "VsixFootie",
+//        "VsixHelloWorldCommandButton",
+//        "VsixHelloWorldPopUp",
+//        "VsixHelloWorldToolBar",
+//        "VsixRatingChaser",
+//        "VsixToolWindowAsyncPackageExample",
+//        "VsixTwitterWidget",
+//        "VstsDashboardWidgetProjectTemplate",
+//        "WpfAsyncBindingPropertyExample",
+//    ];
 
-    repos.forEach(function (repoFolderName) {
-        var badgesMarkdown = GetBadgesMarkdown();
-        ReplaceBadgeComment(repoFolderName, badgesMarkdown);
-    });
+//    repos.forEach(function (repoFolderName) {
+//        var badgesMarkdown = GetBadgesMarkdown();
+//        ReplaceBadgeComment(repoFolderName, badgesMarkdown);
+//    });
 
-    function GetBadgesMarkdown() {
-        var lineBreak = '\n';
+//    function GetBadgesMarkdown() {
+//        var lineBreak = '\n';
 
-        var multipleBadgesMarkdown = GetMultipleBadgesMarkdown();
+//        var multipleBadgesMarkdown = GetMultipleBadgesMarkdown();
 
-        var badgesMarkdownFull = badgeCommentStart + lineBreak + multipleBadgesMarkdown + badgeCommentEnd;
+//        var badgesMarkdownFull = badgeCommentStart + lineBreak + multipleBadgesMarkdown + badgeCommentEnd;
 
-        return badgesMarkdownFull;
+//        return badgesMarkdownFull;
 
-        function GetMultipleBadgesMarkdown() {
-            let badgesMarkdownFinal;
+//        function GetMultipleBadgesMarkdown() {
+//            let badgesMarkdownFinal;
 
-            var badgesMarkdown =
-                [
-                    "badge1aaaa",
-                    "badge2bbb",
-                    "badge3cccc"
-                ];
+//            var badgesMarkdown =
+//                [
+//                    "badge1aaaa",
+//                    "badge2bbb",
+//                    "badge3cccc"
+//                ];
 
-            badgesMarkdown.forEach(function (badgeMarkdown) {
-                badgesMarkdownFinal += badgeMarkdown + lineBreak;
-            });
+//            badgesMarkdown.forEach(function (badgeMarkdown) {
+//                badgesMarkdownFinal += badgeMarkdown + lineBreak;
+//            });
 
-            return badgesMarkdownFinal;
-        }
-    }
+//            return badgesMarkdownFinal;
+//        }
+//    }
 
-    function ReplaceBadgeComment(repoFolderName, badgesMarkdown) {
-        var destination = "../../../" + repoFolderName;
-        var source = destination + "/ReadMe.md";
-        var matchAnyCharacter = '[^]+';//"don't match no characters" i.e. a double negative that can re-read as "match any character" i.e. even including line breaks
-        var badgesRegex = new RegExp(badgeCommentStart + matchAnyCharacter + badgeCommentEnd, 'g');
-        gulp.src([source])
-            .pipe(replace(badgesRegex, badgesMarkdown))
-            .pipe(gulp.dest(destination));
-    } 
-}
+//    function ReplaceBadgeComment(repoFolderName, badgesMarkdown) {
+//        var destination = "../../../" + repoFolderName;
+//        var source = destination + "/ReadMe.md";
+//        var matchAnyCharacter = '[^]+';//"don't match no characters" i.e. a double negative that can re-read as "match any character" i.e. even including line breaks
+//        var badgesRegex = new RegExp(badgeCommentStart + matchAnyCharacter + badgeCommentEnd, 'g');
+//        gulp.src([source])
+//            .pipe(replace(badgesRegex, badgesMarkdown))
+//            .pipe(gulp.dest(destination));
+//    } 
+//}
 
 
 //vsixratingchaser NugetREADME.md
--- Src/ReadMeMagic/gulpfile2.ts
+++ Src/ReadMeMagic/gulpfile2.ts
@@ -1,7 +1,90 @@
 import * as gulp from 'gulp';
 
 gulp.task('typscr2b', () => {
-    console.log('Gulp is running2222222!');
+    console.log('Gulp is running 333!');
+
+    var replace = require('gulp-string-replace');
+
+    //Common variables
+    let prefix: string = "Badges";
+    let htmlCommentStart: string  = "<!--" + prefix;
+    let htmlCommentEnd: string  = "-->";
+    let badgeCommentStart: string  = htmlCommentStart + "START" + htmlCommentEnd;
+    let badgeCommentEnd: string  = htmlCommentStart + "END" + htmlCommentEnd;
+
+    ReplaceBadgeComments();
+
+    function ReplaceBadgeComments() {
+
+        //tsnode(dummy());
+
+        var repos = [
+            "AutoFindReplace",
+            //"Badges-playground",
+            "DotNetFlags",
+            "FilesForEveryExtensionCreator",
+            "HelloWorldVstsExtension",
+            "OpenInApp.Launcher",
+            "QuickLaunchButtons",
+            "Quiz.Launcher",
+            "SolutionOpenPopUp",
+            "TrivialApisForIDE",
+            "VisualStudioMarketplaceMetrics",
+            "VsixFootie",
+            "VsixHelloWorldCommandButton",
+            "VsixHelloWorldPopUp",
+            "VsixHelloWorldToolBar",
+            "VsixRatingChaser",
+            "VsixToolWindowAsyncPackageExample",
+            "VsixTwitterWidget",
+            "VstsDashboardWidgetProjectTemplate",
+            "WpfAsyncBindingPropertyExample",
+        ];
+
+        repos.forEach(function (repoFolderName) {
+            var badgesMarkdown = GetBadgesMarkdown();
+            ReplaceBadgeComment(repoFolderName, badgesMarkdown);
+        });
+
+        function GetBadgesMarkdown() {
+            var lineBreak = '\n';
+
+            var multipleBadgesMarkdown = GetMultipleBadgesMarkdown();
+
+            var badgesMarkdownFull = badgeCommentStart + lineBreak + multipleBadgesMarkdown + badgeCommentEnd;
+
+            return badgesMarkdownFull;
+
+            function GetMultipleBadgesMarkdown() {
+                let badgesMarkdownFinal:string = "";
+
+                var badgesMarkdown =
+                    [
+                        "badge1aaaa",
+                        "badge2bbb",
+                        "badge2bbbeeeeeee",
+                        "badge3cccc"
+                    ];
+
+                badgesMarkdown.forEach(function (badgeMarkdown) {
+                    badgesMarkdownFinal += badgeMarkdown + lineBreak;
+                });
+
+                return badgesMarkdownFinal;
+            }
+        }
+
+        function ReplaceBadgeComment(repoFolderName:string, badgesMarkdown:string) {
+            let destination :string = "../../../" + repoFolderName;
+            let source: string  = destination + "/ReadMe.md";
+            let matchAnyCharacter: string  = '[^]+';//"don't match no characters" i.e. a double negative that can re-read as "match any character" i.e. even including line breaks
+            var badgesRegex = new RegExp(badgeCommentStart + matchAnyCharacter + badgeCommentEnd, 'g');
+            gulp.src([source])
+                .pipe(replace(badgesRegex, badgesMarkdown))
+                .pipe(gulp.dest(destination));
+        }
+    }
+
 });
 
 class Student {
