f55c0bccfa8464c80cf2862ca1cee53e202926dc
Greg Trevellick <gtrevellick@googlemail.com>
2018-08-01 22:40:44 +0100

    .

---
+++ Src/ReadMeMagic/UnitTests/zzz-boot-tests.ts
@@ -0,0 +1,17 @@
+﻿//// There's no typing for the `__karma__` variable. Just declare it as any
+//declare var __karma__: any;
+//declare var require: any;
+
+//// Prevent Karma from running prematurely
+//// tslint:disable-next-line:no-empty
+//__karma__.loaded = () => { };
+
+//// Then we find all the tests
+////const context = require.context("../", true, /\.spec\.ts$/);
+//const context = require.context("../", true, /\.js$/);
+
+//// And load the modules
+//context.keys().map(context);
+
+//// Finally, start Karma to run the tests
+//__karma__.start();
