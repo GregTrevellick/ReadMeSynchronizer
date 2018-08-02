9cb3d2a40edb3785e4473279dd018eac87907973
Greg Trevellick <gtrevellick@googlemail.com>
2018-08-02 07:38:32 +0100

    .

--- Src/ReadMeMagic/GulpTasks/run_tests_task.ts
+++ Src/ReadMeMagic/GulpTasks/run_tests_task.ts
@@ -3,5 +3,5 @@ import * as gulp from 'gulp';
 const run = require("gulp-run");
 
 gulp.task("run_karma_tests", function () {
-    return run("karma start ./UnitTests/karma.conf.js").exec();
+    return run("karma start ./UnitTestConfig/karma.conf.js").exec();
 });
--- Src/ReadMeMagic/Gulpfile.js
+++ Src/ReadMeMagic/Gulpfile.js
@@ -1,10 +1,8 @@
 ﻿/// <binding AfterBuild='default' ProjectOpened='default' />
-
 //"use strict";
-
 var gulp = require('gulp');
-///////////////////////////var bower = require('gulp-bower');
+//var bower = require('gulp-bower');
 var Server = require('karma').Server;
 //var tsnode = require('ts-node');
-require('./GulpTasks/update_badges_task.ts');
+require('./GulpTasks/update_readme_files_task.ts');
 require('./GulpTasks/run_tests_task.ts');
