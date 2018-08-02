53704dec3eed43f95b95fec09d46af551e7c5f65
Greg Trevellick <gtrevellick@googlemail.com>
2018-08-02 07:55:49 +0100

    .

--- Src/ReadMeMagic/Gulpfile.js
+++ Src/ReadMeMagic/Gulpfile.js
@@ -1,8 +1,12 @@
 ﻿/// <binding AfterBuild='default' ProjectOpened='default' />
 //"use strict";
 var gulp = require('gulp');
+const run = require("gulp-run");
 //var bower = require('gulp-bower');
 var Server = require('karma').Server;
 //var tsnode = require('ts-node');
 require('./GulpTasks/update_readme_files_task.ts');
 require('./GulpTasks/run_tests_task.ts');
+
+gulp.task("lint", () => run("npm run lint").exec());
+gulp.task("lint_fix", () => run("npm run lint -- --fix").exec());
