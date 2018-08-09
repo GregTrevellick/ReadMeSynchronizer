0e6025661bad5d82172db41af5af1a534aa74f0c
Greg Trevellick <gtrevellick@googlemail.com>
2018-08-09 20:31:25 +0100

    .

--- Src/ReadMeMagic/App/UpdateReadmeFiles/MagicStrings.ts
+++ Src/ReadMeMagic/App/UpdateReadmeFiles/MagicStrings.ts
@@ -51,26 +51,3 @@ export enum VsixHelloWorlds {
     VsixToolWindowAsyncPackageExample = "VsixToolWindowAsyncPackageExample",
     WpfAsyncBindingPropertyExample = "WpfAsyncBindingPropertyExample",
 }
-
-//export enum RepoNames {
-//    //AutoFindReplace = "AutoFindReplace",
-//    //BadgesPlayground = "Badges-playground",
-//    //DotNetFlags = "DotNetFlags",
-//    //FilesForEveryExtensionCreator = "FilesForEveryExtensionCreator",
-//    HelloWorldVstsExtension = "HelloWorldVstsExtension",
-//    //OpenInAppLauncher = "OpenInApp.Launcher",
-//    //QuickLaunchButtons = "QuickLaunchButtons",
-//    //QuizLauncher = "Quiz.Launcher",
-//    //SolutionOpenPopUp = "SolutionOpenPopUp",
-//    //TrivialApisForIDE = "TrivialApisForIDE",
-//    //VisualStudioMarketplaceMetrics = "VisualStudioMarketplaceMetrics",
-//    //VsixFootie = "VsixFootie",
-//    VsixHelloWorldCommandButton = "VsixHelloWorldCommandButton",
-//    VsixHelloWorldPopUp = "VsixHelloWorldPopUp",
-//    VsixHelloWorldToolBar = "VsixHelloWorldToolBar",
-//    //VsixRatingChaser = "VsixRatingChaser",
-//    VsixToolWindowAsyncPackageExample = "VsixToolWindowAsyncPackageExample",
-//    //VsixTwitterWidget = "VsixTwitterWidget",
-//    //VstsDashboardWidgetProjectTemplate = "VstsDashboardWidgetProjectTemplate",
-//    WpfAsyncBindingPropertyExample = "WpfAsyncBindingPropertyExample",
-//}
