2b0eefd8ccab95a0287d05488f189d6e7aaca8fc
Greg Trevellick <gtrevellick@googlemail.com>
2018-08-16 21:55:39 +0100

    .

--- Src/ReadMeSynchronizer/Gulpfile.js
+++ Src/ReadMeSynchronizer/Gulpfile.js
@@ -2,11 +2,11 @@
 
 //"use strict";
 
-let gulp = require('gulp');
+const gulp = require('gulp');
 const run = require("gulp-run");
-let Server = require('karma').Server;
+const Server = require('karma').Server;
 
 require('./GulpTasks/GitTasks.ts');
+require('./GulpTasks/MarkdownTasks.ts');
 gulp.task("Lint", () => run("npm run lint").exec());
 gulp.task("Lint_Fix", () => run("npm run lint -- --fix").exec());
-require('./GulpTasks/MarkdownTasks.ts');
