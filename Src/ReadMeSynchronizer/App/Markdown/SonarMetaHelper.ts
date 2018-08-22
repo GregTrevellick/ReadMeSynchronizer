6eacb6eb316dff6fc51f1628d7ec9a66ad3e3db8
Greg Trevellick <gtrevellick@googlemail.com>
2018-08-22 14:21:36 +0100

    .

--- Src/ReadMeSynchronizer/App/Markdown/SonarMetaHelper.ts
+++ Src/ReadMeSynchronizer/App/Markdown/SonarMetaHelper.ts
@@ -5,7 +5,7 @@ export class SonarMetaHelper {
 
     private prefixComponentMeasures = "https://sonarcloud.io/component_measures?id=";
     private prefixDashboard = "https://sonarcloud.io/dashboard?id=";
-    private prefixProject = "https://sonarcloud.io/project/issues?id=";
+    //////////////////////////////////////////////////////////////////////////////////////////////private prefixProject = "https://sonarcloud.io/project/issues?id=";
     private sonarMetaData: SonarMetaData;
 
     constructor() {
@@ -18,67 +18,67 @@ export class SonarMetaHelper {
             case SonarCategory.QualityGateStatus: {
                 this.sonarMetaData.badgeQueryString = "&metric=alert_status";
                 this.sonarMetaData.badgeHyperlinkTargetUrlPrefix = this.prefixDashboard;
-                this.sonarMetaData.badgeHyperlinkTargetUrlSuffix = "&resolved=false&types=ALERT_STATUS";
+                this.sonarMetaData.badgeHyperlinkTargetUrlSuffix = "";
                 break;
             }
             case SonarCategory.Bugs: {
                 this.sonarMetaData.badgeQueryString = "&metric=bugs";
                 this.sonarMetaData.badgeHyperlinkTargetUrlPrefix = this.prefixComponentMeasures;
-                this.sonarMetaData.badgeHyperlinkTargetUrlSuffix = "&resolved=false&types=BUG";   
+                this.sonarMetaData.badgeHyperlinkTargetUrlSuffix = "&metric=bugs";
                 break;
             }
             case SonarCategory.CodeSmells: {
                 this.sonarMetaData.badgeQueryString = "&metric=code_smells";
-                this.sonarMetaData.badgeHyperlinkTargetUrlPrefix = this.prefixProject;
-                this.sonarMetaData.badgeHyperlinkTargetUrlSuffix = "&resolved=false&types=CODE_SMELL"; 
+                this.sonarMetaData.badgeHyperlinkTargetUrlPrefix = this.prefixComponentMeasures;
+                this.sonarMetaData.badgeHyperlinkTargetUrlSuffix = "&metric=code_smells";
                 break;
             }
             case SonarCategory.Coverage: {
                 this.sonarMetaData.badgeQueryString = "&metric=coverage";
                 this.sonarMetaData.badgeHyperlinkTargetUrlPrefix = this.prefixComponentMeasures;
-                this.sonarMetaData.badgeHyperlinkTargetUrlSuffix = "&resolved=false&types=COVERAGE"; 
+                this.sonarMetaData.badgeHyperlinkTargetUrlSuffix = "&metric=Coverage";
                 break;
             }
             case SonarCategory.DuplicatedLinesDensity: {
                 this.sonarMetaData.badgeQueryString = "&metric=duplicated_lines_density";
                 this.sonarMetaData.badgeHyperlinkTargetUrlPrefix = this.prefixComponentMeasures;
-                this.sonarMetaData.badgeHyperlinkTargetUrlSuffix = "&resolved=false&types=DUPLICATED_LINES_DENSITY";
+                this.sonarMetaData.badgeHyperlinkTargetUrlSuffix = "&metric=duplicated_lines";
                 break;
             }
             case SonarCategory.NumberOfLinesOfCode: {
                 this.sonarMetaData.badgeQueryString = "&metric=ncloc";
                 this.sonarMetaData.badgeHyperlinkTargetUrlPrefix = this.prefixComponentMeasures;
-                this.sonarMetaData.badgeHyperlinkTargetUrlSuffix = "&resolved=false&types=NCLOC";
+                this.sonarMetaData.badgeHyperlinkTargetUrlSuffix = "&metric=ncloc";
                 break;
             }
             case SonarCategory.ReliabilityRating: {
                 this.sonarMetaData.badgeQueryString = "&metric=reliability_rating";
                 this.sonarMetaData.badgeHyperlinkTargetUrlPrefix = this.prefixComponentMeasures;
-                this.sonarMetaData.badgeHyperlinkTargetUrlSuffix = "&resolved=false&types=RELIABILITY_RATING";
+                this.sonarMetaData.badgeHyperlinkTargetUrlSuffix = "&metric=reliability_rating";
                 break;
             }
             case SonarCategory.SecurityRating: {
                 this.sonarMetaData.badgeQueryString = "&metric=security_rating";
                 this.sonarMetaData.badgeHyperlinkTargetUrlPrefix = this.prefixComponentMeasures;
-                this.sonarMetaData.badgeHyperlinkTargetUrlSuffix = "&resolved=false&types=SECURITY_RATING";
+                this.sonarMetaData.badgeHyperlinkTargetUrlSuffix = "&metric=security_rating";
                 break;
             }
             case SonarCategory.TechnicalDebt: {
                 this.sonarMetaData.badgeQueryString = "&metric=sqale_index";
-                this.sonarMetaData.badgeHyperlinkTargetUrlPrefix = this.prefixProject;
-                this.sonarMetaData.badgeHyperlinkTargetUrlSuffix = "&resolved=false&types=SQALE_INDEX";/////////////////////////////////not ok
+                this.sonarMetaData.badgeHyperlinkTargetUrlPrefix = this.prefixComponentMeasures;
+                this.sonarMetaData.badgeHyperlinkTargetUrlSuffix = "&metric=sqale_index";
                 break;
             }
             case SonarCategory.Maintainability: {
                 this.sonarMetaData.badgeQueryString = "&metric=sqale_rating";
-                this.sonarMetaData.badgeHyperlinkTargetUrlPrefix = this.prefixProject;
-                this.sonarMetaData.badgeHyperlinkTargetUrlSuffix = "&resolved=false&types=SQALE_RATING";////////////////////////////////not ok
+                this.sonarMetaData.badgeHyperlinkTargetUrlPrefix = this.prefixComponentMeasures;
+                this.sonarMetaData.badgeHyperlinkTargetUrlSuffix = "&metric=sqale_rating";
                 break;
             }
             case SonarCategory.Vulnerabilities: {
                 this.sonarMetaData.badgeQueryString = "&metric=vulnerabilities";
-                this.sonarMetaData.badgeHyperlinkTargetUrlPrefix = this.prefixProject;
-                this.sonarMetaData.badgeHyperlinkTargetUrlSuffix = "&resolved=false&types=VULNERABILITY";
+                this.sonarMetaData.badgeHyperlinkTargetUrlPrefix = this.prefixComponentMeasures;
+                this.sonarMetaData.badgeHyperlinkTargetUrlSuffix = "&metric=vulnerabilities";
                 break;
             }
         }
