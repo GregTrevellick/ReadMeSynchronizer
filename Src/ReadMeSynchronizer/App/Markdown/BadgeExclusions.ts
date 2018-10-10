b4cf2519718a70178315523c7120255cff6c5ce5
Greg Trevellick <gtrevellick@googlemail.com>
2018-10-10 21:19:34 +0100

    .

--- Src/ReadMeSynchronizer/App/Markdown/BadgeExclusions.ts
+++ Src/ReadMeSynchronizer/App/Markdown/BadgeExclusions.ts
@@ -4,17 +4,24 @@ export class BadgeExclusions {
 
     public appveyor: RepoNames[] = [
         //RepoNames.AllBadges,
+        RepoNames.AngularBasic,
         RepoNames.awib,
         RepoNames.MiscellaneousArtefacts,
         RepoNames.More,
     ];
 
     public azurePipeline: RepoNames[] = [
-        //RepoNames.AllBadges,
+        RepoNames.AllBadges,
+        RepoNames.AngularBasic,
         RepoNames.awib,
+        RepoNames.BrowserExtensionUsingAngularWorksInChromeNotInEdge,
+        //RepoNames.HelloWorldTypescriptParcel,
+        //RepoNames.HelloWorldTypescriptWebpack,
+        //RepoNames.HelloWorldVstsExtension,
         RepoNames["Menees.VsTools.2017"],
         RepoNames.MiscellaneousArtefacts,
         RepoNames.More,
+        RepoNames.OpenCoverExample,
     ];
 
     //betterCodeHubCompliance
