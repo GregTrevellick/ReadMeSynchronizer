c7ed4cd4492c40cc688fea1f6cfff2bee7e722d8
Greg Trevellick <gtrevellick@googlemail.com>
2018-10-10 21:04:24 +0100

    .

--- Src/ReadMeSynchronizer/App/Markdown/BadgeExclusions.ts
+++ Src/ReadMeSynchronizer/App/Markdown/BadgeExclusions.ts
@@ -3,13 +3,18 @@ import { RepoNames } from "./RepoNames";
 export class BadgeExclusions {
 
     public appveyor: RepoNames[] = [
+        //RepoNames.AllBadges,
+        RepoNames.awib,
+        RepoNames.MiscellaneousArtefacts,
         RepoNames.More,
-        RepoNames.MiscellaneousArtefacts
     ];
 
     public azurePipeline: RepoNames[] = [
+        //RepoNames.AllBadges,
         RepoNames.awib,
-        RepoNames["Menees.VsTools.2017"]
+        RepoNames["Menees.VsTools.2017"],
+        RepoNames.MiscellaneousArtefacts,
+        RepoNames.More,
     ];
 
     //betterCodeHubCompliance
