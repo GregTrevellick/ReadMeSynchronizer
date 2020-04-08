1e29f45ccfb2037d45f94b53454f9a0c90128c21
Greg Trevellick <gtrevellick@googlemail.com>
2020-04-08 22:01:55 +0100

    .

--- Src/ReadMeSynchronizer/GulpTasks/MarkdownTasks.ts
+++ Src/ReadMeSynchronizer/GulpTasks/MarkdownTasks.ts
@@ -1,16 +1,7 @@
 import * as gulp from "gulp";
 import { ReadMeUpdater } from "../App/Markdown/UpdateReadMeFiles";
 
-///////////////////////////////{ allowEmpty: true }
-
-
-
-//https://github.com/sindresorhus/del/issues/45
-
-
-
 gulp.task("Update_Markdown_Badges", () => {
-    /////////////////////////////////gulp.src("D:/_Dgit/_MINE_ACTIVE/VstsDashboardWidgetProjectTemplate/README.md ", { allowEmpty: true });
     const readMeUpdater: ReadMeUpdater = new ReadMeUpdater();
     readMeUpdater.ReplaceBadgeComments();
 });
