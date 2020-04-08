36ea84c15cf54d0d78be202335e32142417e8c24
Greg Trevellick <gtrevellick@googlemail.com>
2020-04-08 22:01:06 +0100

    .

--- Src/ReadMeSynchronizer/GulpTasks/MarkdownTasks.ts
+++ Src/ReadMeSynchronizer/GulpTasks/MarkdownTasks.ts
@@ -1,21 +1,16 @@
 import * as gulp from "gulp";
 import { ReadMeUpdater } from "../App/Markdown/UpdateReadMeFiles";
 
-//{ allowEmpty: true }
+///////////////////////////////{ allowEmpty: true }
+
+
+
+//https://github.com/sindresorhus/del/issues/45
+
+
 
 gulp.task("Update_Markdown_Badges", () => {
-    gulp.src([], { allowEmpty: true });
+    /////////////////////////////////gulp.src("D:/_Dgit/_MINE_ACTIVE/VstsDashboardWidgetProjectTemplate/README.md ", { allowEmpty: true });
     const readMeUpdater: ReadMeUpdater = new ReadMeUpdater();
     readMeUpdater.ReplaceBadgeComments();
 });
-
-//gulp.task('vendor', () =>
-//    gulp.src([
-//        'node_modules/react/dist/react.min.js',
-//        'node_modules/react-dom/dist/react-dom.min.js',
-//        'node_modules/react/umd/react.production.min.js',
-//        'node_modules/react-dom/umd/react-dom.production.min.js'
-//    ], { allowEmpty: true })
-//        .pipe(rename(path => path.basename = path.basename.replace(/\.production/, '')))
-//        .pipe(gulp.dest('vendor'))
-//);
