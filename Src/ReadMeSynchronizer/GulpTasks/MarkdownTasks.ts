94e102195ced2b63bbee16cd9689e93f4db6cbd7
Greg Trevellick <gtrevellick@googlemail.com>
2020-06-12 23:29:44 +0100

    fix "Did you forget to signal async completion?"     add a simple done();  promise that you got away with pre gulp 4 but not after gulp 4

--- Src/ReadMeSynchronizer/GulpTasks/MarkdownTasks.ts
+++ Src/ReadMeSynchronizer/GulpTasks/MarkdownTasks.ts
@@ -1,7 +1,8 @@
 import * as gulp from "gulp";
 import { ReadMeUpdater } from "../App/Markdown/UpdateReadMeFiles";
 
-gulp.task("Update_Markdown_Badges", () => {
+gulp.task("Update_Markdown_Badges", function (done) {
     const readMeUpdater: ReadMeUpdater = new ReadMeUpdater();
     readMeUpdater.ReplaceBadgeComments();
+    done();
 });
